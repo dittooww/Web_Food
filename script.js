8add9b15e4bfedbc2168062b4515e47056832ec1
Radhitya <rgustianputra@gmail.com>
2024-11-07 13:31:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("orderbutton").onclick = function () {
+    alert("LAGI DIPROSES")
+    let foodprice = parseInt(document,getElementById ("food").value);
+    let drinkprice = parseInt(document,getElementById ("drink").value);
+    let totalprice = foodprice + drinkprice;
+    let discount = 0;
+    if (totalprice > 10){
+        diskon
+        alert("ambasaing")
+    }
+    
+}
